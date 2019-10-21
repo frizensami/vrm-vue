79599679a3514b66f1ac46b69b5ea372014317c1
Sriram Sami <frizensami@gmail.com>
2019-10-21 22:10:29 +0800

    Add Graph component - 3d-force-graph basic example

---
+++ src/components/Graph.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="graph">ASDF</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ForceGraph3D from '3d-force-graph'
+
+export default Vue.extend({
+  name: 'Graph',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      graph: ForceGraph3D()
+    }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      // Random tree
+      const N = 300
+      const gData = {
+        nodes: [...Array(N).keys()].map(i => ({ id: i })),
+        links: [...Array(N).keys()]
+          .filter(id => id)
+          .map(id => ({
+            source: id,
+            target: Math.round(Math.random() * (id - 1))
+          }))
+      }
+      this.graph(this.$el).graphData(gData)
+    })
+  }
+
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Visual Research Management"/>
+    <Graph/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Graph from '@/components/Graph.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Graph
   }
 }
 </script>
