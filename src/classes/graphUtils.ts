398526276f4a42d8adac5ca44db2b4ca273d910a
Sriram Sami <frizensami@gmail.com>
2019-10-25 22:38:33 +0800

    True dynamic coloring based on group

--- src/classes/graphUtils.ts
+++ src/classes/graphUtils.ts
@@ -12,14 +12,20 @@ export default class GraphUtils {
       .nodeId('id')
       .nodeLabel('title')
       .nodeVal('userSignificance')
-      .nodeAutoColorBy('group')
+      .nodeColor(GraphUtils.nodeToColor)
   }
 
   public static updateGraphData (graph: any, id: any, property: any, oldVal: any, newVal: any) {
     let graphData = graph.graphData()
     for (let node of graphData.nodes) {
       if (node.id === id) {
+        if (property === 'group') {
+          delete node.color
+          delete node.materialcolor
+        }
         node[property] = newVal
+        console.log(node)
+        // If property is 'group' - recolor
         graph.refresh()
       }
     }
@@ -48,4 +54,57 @@ export default class GraphUtils {
 
     return graphData
   }
+
+  private static nodeToColor (node: any) {
+    return GraphUtils.stringToColor3(node.group)
+  }
+  private static stringToColor (str: String) {
+    return GraphUtils.intToRGB(GraphUtils.hashCode(str))
+  }
+
+  private static stringToColor2 (str: String) {
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash)
+    }
+    let color = '#'
+    for (let i = 0; i < 3; i++) {
+      let value = (hash >> (i * 8)) & 0xFF
+      color += ('00' + value.toString(16)).substr(-2)
+    }
+    return color
+  }
+
+  private static stringToColor3 (str: String) {
+    return GraphUtils.intToHSL(GraphUtils.hashCode2(str))
+  }
+
+  private static hashCode (str: String) { // java String#hashCode
+    var hash = 0
+    for (var i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash)
+    }
+    return hash
+  }
+
+  private static hashCode2 (str: String) {
+    return str.split('').reduce((prevHash, currVal) =>
+      (((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0)
+  }
+
+  private static intToRGB (i: number) {
+    var c = (i & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase()
+
+    return '00000'.substring(0, 6 - c.length) + c
+  }
+
+  private static intToHSL = function (val: number) {
+    // saturation is how rich you want the color,
+    // lightness determines the deepness:
+    //    50% is normal, 25% is dark colors, 75% is pastel.
+    var shortened = val % 360
+    return 'hsl(' + shortened + ',100%, 75%)'
+  };
 }
